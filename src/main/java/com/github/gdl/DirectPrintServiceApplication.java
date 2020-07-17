b86ebed6d2ded17a6fb44c1c1c969a589e31d570
thawat thammawong <thawat.thammawong@gmail.com>
2020-07-17 23:23:05 +0700

    Add method restart

--- src/main/java/com/github/gdl/DirectPrintServiceApplication.java
+++ src/main/java/com/github/gdl/DirectPrintServiceApplication.java
@@ -1,13 +1,42 @@
 package com.github.gdl;
 
+import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 @SpringBootApplication
+@RestController
 public class DirectPrintServiceApplication {
+    private static ConfigurableApplicationContext context;
 
     public static void main(String[] args) {
-        SpringApplication.run(DirectPrintServiceApplication.class, args);
+        context = SpringApplication.run(DirectPrintServiceApplication.class, args);
+    }
+
+    public static void restart() {
+        ApplicationArguments args = context.getBean(ApplicationArguments.class);
+        Thread thread = new Thread(() -> {
+            context.close();
+            context = SpringApplication.run(DirectPrintServiceApplication.class, args.getSourceArgs());
+        });
+        thread.setDaemon(false);
+        thread.start();
+    }
+
+    @GetMapping("/restart")
+    public void restarts() {
+        DirectPrintServiceApplication.restart();
+    }
+
+    @GetMapping("/")
+    public void root(HttpServletResponse response) throws IOException {
+        response.sendRedirect("/swagger-ui.html");
     }
 
 }
