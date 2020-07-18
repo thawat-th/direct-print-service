0d390ac1fcc64b159eb5d7cc9e1321c043436fc4
thawat thammawong <thawat.thammawong@gmail.com>
2020-07-18 11:58:09 +0700

    Migrate springfox to openapi

--- src/main/java/com/github/gdl/DirectPrintController.java
+++ src/main/java/com/github/gdl/DirectPrintController.java
@@ -1,13 +1,12 @@
 package com.github.gdl;
 
 
-import com.google.common.io.ByteStreams;
-import io.swagger.annotations.ApiOperation;
-import org.apache.tomcat.util.codec.binary.Base64;
+import io.swagger.v3.oas.annotations.Operation;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -21,6 +20,7 @@ import java.io.IOException;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Arrays;
+import java.util.Base64;
 
 @RestController
 @RequestMapping(value = "/v1")
@@ -29,41 +29,41 @@ public class DirectPrintController {
     private static final Logger log = LoggerFactory.getLogger(DirectPrintController.class);
 
     @GetMapping("/printers")
-    @ApiOperation(value = "Available printers")
+    @Operation(summary = "Available printers")
     public ResponseEntity printers() {
         PrintService[] services = PrintServiceLookup.lookupPrintServices(null, null);
         log.info("Available printers: " + Arrays.asList(services));
-        return new ResponseEntity<>( Arrays.stream(services).map(PrintService::getName), HttpStatus.OK);
+        return new ResponseEntity<>(Arrays.stream(services).map(PrintService::getName), HttpStatus.OK);
     }
 
     @GetMapping("/printer/default")
-    @ApiOperation(value = "Get Default printer")
+    @Operation(summary = "Get Default printer")
     public ResponseEntity defaults() {
         PrintService service = PrintServiceLookup.lookupDefaultPrintService();
         return new ResponseEntity<>(service.getName(), HttpStatus.OK);
     }
 
-    @PostMapping("/print")
-    @ApiOperation(value = "Printing")
-    public ResponseEntity print( @RequestParam @NotNull MultipartFile file) throws IOException, PrintException {
+    @PostMapping(value = "/print", consumes = "multipart/form-data")
+    @Operation(summary = "Printing")
+    public ResponseEntity print(@RequestParam @NotNull MultipartFile file) throws IOException, PrintException {
         this.printJob(file.getBytes());
         return new ResponseEntity<>("", HttpStatus.OK);
     }
 
-    @PostMapping("/print/binary")
-    @ApiOperation(value = "Printing a base64 encoded")
+    @PostMapping(value = "/print/binary", consumes = "multipart/form-data")
+    @Operation(summary = "Printing a base64 encoded")
     public ResponseEntity printBinary(@RequestParam @NotNull String data) throws PrintException {
-        byte[] bytes  = Base64.decodeBase64(data);
+        byte[] bytes = Base64.getDecoder().decode(data);
         this.printJob(bytes);
         return new ResponseEntity<>("", HttpStatus.OK);
     }
 
     @PostMapping("/print/url")
-    @ApiOperation(value = "Print form URL")
+    @Operation(summary = "Print form URL")
     public ResponseEntity printUri(@RequestParam @NotNull String url) throws PrintException, IOException {
         URLConnection urlConnection = new URL(url).openConnection();
         urlConnection.addRequestProperty("User-Agent", "Mozilla/4.0");
-        byte[] bytes  = ByteStreams.toByteArray(urlConnection.getInputStream());
+        byte[] bytes = StreamUtils.copyToByteArray(urlConnection.getInputStream());
         this.printJob(bytes);
         return new ResponseEntity<>("", HttpStatus.OK);
     }
